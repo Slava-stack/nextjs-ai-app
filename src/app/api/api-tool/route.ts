35b2063720830221768b89c2e510c891eb70e8ef
Slava <minielephant16@gmail.com>
2025-08-30 02:59:13 +0300

    added weather api interactions

---
+++ src/app/api/api-tool/route.ts
@@ -0,0 +1,63 @@
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  tool,
+  InferUITools,
+  UIDataTypes,
+  stepCountIs,
+} from "ai";
+import { openai } from "@ai-sdk/openai";
+import { z } from "zod";
+import "dotenv/config";
+
+const tools = {
+  getWeather: tool({
+    description: "Get the weather for a location",
+    inputSchema: z.object({
+      city: z.string().describe("The city to get the weather for"),
+    }),
+    execute: async ({ city }) => {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}`
+      );
+      const data = await response.json();
+      const weatherData = {
+        location: {
+          name: data.location.name,
+          country: data.location.country,
+          localtime: data.location.localtime,
+        },
+        current: {
+          temp_c: data.current.temp_c,
+          condition: {
+            text: data.current.condition.text,
+            code: data.current.condition.code,
+          },
+        },
+      };
+      return weatherData;
+    },
+  }),
+};
+
+export type ChatTools = InferUITools<typeof tools>;
+export type ChatMessage = UIMessage<never, UIDataTypes, ChatTools>;
+
+export async function POST(req: Request) {
+  try {
+    const { messages }: { messages: ChatMessage[] } = await req.json();
+
+    const result = streamText({
+      model: openai("gpt-4.1-nano-2025-04-14"),
+      messages: convertToModelMessages(messages),
+      tools,
+      stopWhen: stepCountIs(2),
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (err) {
+    console.error("Error streaming chat completion:", err);
+    return new Response("Failed to stream chat completion", { status: 500 });
+  }
+}
